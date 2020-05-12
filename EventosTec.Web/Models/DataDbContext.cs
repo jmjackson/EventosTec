00633f45cd1010780075e638c89216ffb99174af
Juan Hernandez <jarceluz@gmail.com>
2020-05-12 14:16:26 -0700

    Event Model

--- EventosTec.Web/Models/DataDbContext.cs
+++ EventosTec.Web/Models/DataDbContext.cs
@@ -16,5 +16,6 @@ namespace EventosTec.Web.Models
 
         public DbSet<Client> Clients { get; set; }
         public DbSet<City> Cities { get; set; }
+        public DbSet<Event> Events { get; set; }
     }
 }
